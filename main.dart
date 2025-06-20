8bc9a9a5b67129a1149d49850a8b4807b80ec06d
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-20 16:34:08 +0200

    Tipus de dades bàsiques, int,double, num,string, bool.

--- main.dart
+++ main.dart
@@ -1,4 +1,26 @@
-// Comentaris
-void mainz() {  //Si canviem el nom de la funció, main no es pot executar
-  print('Hola desde Dart en mi PC'); // Groc -> Identifiers (jo els hi posso el nom que vull)
+// PRIMITIUS o BÀSICS
+void main() {  
+  // === TypeScript vs Dart: Tipos Básicos ===
+
+// Número entero
+int entero = 30;              // Dart
+
+// Número decimal
+double decimal = 1.75;       // Dart
+
+// Cualquier número (enteros y decimales)
+num numero = 5.5;                // Dart (padre de int y double)
+
+// Texto
+String texte = 'Pedro';    // Dart
+
+// Booleano
+bool booleano = true;         // Dart
+print(entero);
+print(decimal);
+print(numero);
+print(texte);
+print(booleano);
+
+
 }
