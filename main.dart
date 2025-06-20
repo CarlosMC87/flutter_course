3e1add07b76c0bbaa9855244f5f5c044efeb5616
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-20 16:49:28 +0200

    Null

--- main.dart
+++ main.dart
@@ -1,26 +1,10 @@
-// PRIMITIUS o BÀSICS
+// NULL
 void main() {  
-  // === TypeScript vs Dart: Tipos Básicos ===
 
-// Número entero
-int entero = 30;              // Dart
+// Nulo (nullable)
+String? datoNulo = null;           
 
-// Número decimal
-double decimal = 1.75;       // Dart
 
-// Cualquier número (enteros y decimales)
-num numero = 5.5;                // Dart (padre de int y double)
-
-// Texto
-String texte = 'Pedro';    // Dart
-
-// Booleano
-bool booleano = true;         // Dart
-print(entero);
-print(decimal);
-print(numero);
-print(texte);
-print(booleano);
-
+print(datoNulo); // Imprime null
 
 }
