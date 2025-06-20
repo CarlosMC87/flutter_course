2c759fafad5d9ebb7b9543087baae5b2b43c3b27
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-20 17:18:03 +0200

    Object

--- main.dart
+++ main.dart
@@ -1,12 +1,15 @@
-// INFERIT
+// Object
 void main() {          
 
-  
 
-// Tipo inferido
-var ciudadDart = 'Barcelona';    // Dart (inferido como String) como en Javascript
+// Objeto genérico
+Object objetoDart = { "a": 1};    // Dart
+objetoDart = { 2: 1}; // El key puede ser de cualquier tipo
+objetoDart = { 2.1: 1}; // Se pueden reasignar
+objetoDart = { true: 1}; 
+objetoDart = { {"a":1}: 1, "b": 2}; // El key tambien puede ser un objeto
+objetoDart = { "a": 1, "b": 2, "d": {"a":3}}; // Se pueden anidar objetos
 
-ciudadDart = 'Madrid'; // Reasignación de valor, no tipo
-ciudadDart = 123; // Error: Dart no permite cambiar el tipo de una variable una vez asignado
+print(objetoDart); 
 
 }
