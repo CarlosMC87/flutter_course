868f9247fb625adf0a7b75128529bac66782347f
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-20 15:47:02 +0200

    Comentaris, identifiers i keywords.

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
-void main() {
-  print('Hola desde Dart en mi PC');
+// Comentaris
+void main() {  // Blau obscur -> Keywords (reservades per Dart)
+  print('Hola desde Dart en mi PC'); // Groc -> Identifiers (jo els hi posso el nom que vull)
 }
