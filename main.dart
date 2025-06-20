e61e3eaebc2408359a60e230e78e562c947403cc
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-20 16:13:55 +0200

    Mainz no funciona

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
 // Comentaris
-void main() {  // Blau obscur -> Keywords (reservades per Dart)
+void mainz() {  //Si canviem el nom de la funció, main no es pot executar
   print('Hola desde Dart en mi PC'); // Groc -> Identifiers (jo els hi posso el nom que vull)
 }
