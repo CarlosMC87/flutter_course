b563b288dbe58868b8e6fe0060d61a045297dbfe
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-20 17:19:52 +0200

    Constants

--- main.dart
+++ main.dart
@@ -1,15 +1,9 @@
-// Object
-void main() {          
+// CONSTANTS
+void main() {      
 
+// Constante
+const PI_DART = 3.14;  
 
-// Objeto genérico
-Object objetoDart = { "a": 1};    // Dart
-objetoDart = { 2: 1}; // El key puede ser de cualquier tipo
-objetoDart = { 2.1: 1}; // Se pueden reasignar
-objetoDart = { true: 1}; 
-objetoDart = { {"a":1}: 1, "b": 2}; // El key tambien puede ser un objeto
-objetoDart = { "a": 1, "b": 2, "d": {"a":3}}; // Se pueden anidar objetos
-
-print(objetoDart); 
+PI_DART = 2;          // Dart (en tiempo de compilación) no se puede reasignar una constante
 
 }
