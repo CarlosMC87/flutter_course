628ca8f985a1e498bc411b16fe0ddd7ddc366b1b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-26 16:50:24 +0200

    List con data desconocida

--- main.dart
+++ main.dart
@@ -1,7 +1,11 @@
-// List
+// List cuando desconozco el tipo de dato
 void main() {          
 
 // List (como array en Javascript o PHP)
-List<String> frutas = ['Manzana', 'Pera'];
-print(frutas);
+List<Object> datos = ['Texto', 123, false, 3.14];
+List<Object?> datos2 = ['Hola', null, 99, true];
+List<dynamic> datos3 = ['hola', 123, false];
+print(datos);
+print(datos2);
+print(datos3);
 }
