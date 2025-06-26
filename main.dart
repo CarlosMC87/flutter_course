5949417bf3c8e827cd3e88de065554ffd0a92bb0
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-06-26 16:53:19 +0200

    List: acceder a data

--- main.dart
+++ main.dart
@@ -3,9 +3,5 @@ void main() {
 
 // List (como array en Javascript o PHP)
 List<Object> datos = ['Texto', 123, false, 3.14];
-List<Object?> datos2 = ['Hola', null, 99, true];
-List<dynamic> datos3 = ['hola', 123, false];
-print(datos);
-print(datos2);
-print(datos3);
+print(datos[1]);
 }
