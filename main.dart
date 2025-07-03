b26caf24fc0e06155429e0f345f8bb2f511a9e53
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-03 12:31:49 +0200

    Estructures de control de fluxe: ternari amb ramificacions

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
-// Estructures de control de fluxe: ternari
+// Estructures de control de fluxe: ternari amb ramificacions
 
 void main() {
   int edad = 16;
-  String mensaje = (edad >= 18) ? "Mayor" : "Menor";
+  String mensaje = (edad >= 18) ? ((edad > 65)? "Jubilado": "Edad de trabajar") : ((edad < 12) ? "Niño" : "Adolescente");
   print(mensaje); // → Menor
 }
