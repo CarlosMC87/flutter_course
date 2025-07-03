8080bc197b10e0f2bbadf6356befc8391d35dd38
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-03 13:01:17 +0200

    POO: Constructor con parámetros posicionales

--- main.dart
+++ main.dart
@@ -1,23 +1,17 @@
-// Clase
+// Clase: Constructor con parámetros posicionales
 class Persona {
   String nombre;
   int edad;
 
-  // Constructor
   Persona(this.nombre, this.edad);
 
-  int getNombreLength() {
-    return this.nombre.length; // Retorna la longitud del nombre
-  }
-
-  void printLength() {
-    print(
-      "La longitud del nombre es: ${getNombreLength()}",
-    ); // Imprime la longitud del nombre
+  void saludar() {
+    // Método para obtener la longitud del nombre
+    print("Hola, soy $nombre y tengo $edad años.");
   }
 }
 
 void main() {
   var persona = Persona("Pedro", 41); // Crear una instancia de Persona
-  persona.getNombreLength(); // Invocamos el método saludar. -> Hola, soy Pedro y tengo 34 años.
+  persona.saludar(); // Invocamos el método saludar. -> Hola, soy Pedro y tengo 34 años.
 }
